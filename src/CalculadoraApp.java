2b7f13508cd56dde553e21557296ad1247c2bd2f
Arturo Valdez <santiguardado27@gmail.com>
2024-12-18 19:55:18 -0600

    Suma en consola

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CalculadoraApp {
+    public static void main(String[] args) {
+        System.out.println("**** Aplicación Calculadora ****");
+        //System.out.println("Proporciona operacion que deseas realizar: ");
+        System.out.print("Proporciona el primer valor: ");
+        Scanner consola = new Scanner(System.in);
+        var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona el segundo valor: ");
+        var operando2= Integer.parseInt(consola.nextLine());
+        var resultado = operando1 + operando2;
+
+        System.out.println("Resultado: " + resultado);
+    }
 }
