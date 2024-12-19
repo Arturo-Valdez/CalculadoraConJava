de87f0e25b8ad85c1228a9a2e5d5f1c4ad434d3a
Arturo Valdez <santiguardado27@gmail.com>
2024-12-18 20:05:02 -0600

    Menu de operadores

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class CalculadoraApp {
     public static void main(String[] args) {
-        System.out.println("**** Aplicación Calculadora ****");
-        //System.out.println("Proporciona operacion que deseas realizar: ");
-        System.out.print("Proporciona el primer valor: ");
         Scanner consola = new Scanner(System.in);
-        var operando1 = Integer.parseInt(consola.nextLine());
+        System.out.println("**** Aplicación Calculadora ****");
+        //MENU
+        System.out.println("""
+                1.Suma
+                2.Resta
+                3.Division
+                4.Multiplicacion
+                """);
+
+        System.out.println("Proporciona operacion que deseas realizar: ");
+        var operador = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona el primer valor: ");
+        var valor1 = Integer.parseInt(consola.nextLine());
         System.out.print("Proporciona el segundo valor: ");
-        var operando2= Integer.parseInt(consola.nextLine());
-        var resultado = operando1 + operando2;
+        var valor2= Integer.parseInt(consola.nextLine());
+        var resultado = valor1 + valor2;
+
+
 
         System.out.println("Resultado: " + resultado);
     }
