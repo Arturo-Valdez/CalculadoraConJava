726663a587165a35fcabf606b30082a20275444b
Arturo Valdez <santiguardado27@gmail.com>
2024-12-19 19:23:56 -0600

    Fin de proyecto / se agregan funciones para mejor vision de codigo

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -5,45 +5,12 @@ public class CalculadoraApp {
         Scanner consola = new Scanner(System.in);
 
         while (true) {
-        System.out.println("**** Aplicación Calculadora ****");
-        //MENU
-            System.out.println("""
-                    1.Suma
-                    2.Resta
-                    3.Division
-                    4.Multiplicacion
-                    5.Salir
-                    """);
-
-            System.out.print("Proporciona operacion que deseas realizar: ");
+            mostrarMenu();
             try {
                 var operador = Integer.parseInt(consola.nextLine());
                 if (operador >= 1 && operador <= 4) {
-                    System.out.print("Proporciona el primer valor: ");
-                    var valor1 = Integer.parseInt(consola.nextLine());
-                    System.out.print("Proporciona el segundo valor: ");
-                    var valor2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    switch (operador) {
-                        case 1 -> {
-                            resultado = valor1 + valor2;
-                            System.out.println("Resultado Suma: " + resultado);
-                        }
-                        case 2 -> {
-                            resultado = valor1 - valor2;
-                            System.out.println("Resultado Resta: " + resultado);
-                        }
-                        case 3 -> {
-                            resultado = valor1 / valor2;
-                            System.out.println("Resultado Division: " + resultado);
-                        }
-                        case 4 -> {
-                            resultado = valor1 * valor2;
-                            System.out.println("Resultado Multiplicacion: " + resultado);
-                        }
-                        default ->
-                                System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
-                    }
+                    //Ejecutando operacion solicitada
+                    ejecutarOperacion(operador, consola);
                 } else if (operador == 5) {
                     System.out.println("Hasta pronto");
                     break;
@@ -57,4 +24,49 @@ public class CalculadoraApp {
             }
         }//fin de while
     }//fin main
+
+
+    private static void mostrarMenu(){
+        System.out.println("**** Aplicación Calculadora ****");
+        //MENU
+        System.out.println("""
+                    1.Suma
+                    2.Resta
+                    3.Division
+                    4.Multiplicacion
+                    5.Salir
+                    """);
+
+        System.out.print("Proporciona operacion que deseas realizar: ");
+    }//fin funcion mostrarMenu
+
+
+    private  static void ejecutarOperacion(int operador, Scanner consola){
+        System.out.print("Proporciona el primer valor: ");
+        var valor1 = Double.parseDouble(consola.nextLine());
+        System.out.print("Proporciona el segundo valor: ");
+        var valor2 = Double.parseDouble(consola.nextLine());
+        Double resultado;
+        switch (operador) {
+            case 1 -> {
+                resultado = valor1 + valor2;
+                System.out.println("Resultado Suma: " + resultado);
+            }
+            case 2 -> {
+                resultado = valor1 - valor2;
+                System.out.println("Resultado Resta: " + resultado);
+            }
+            case 3 -> {
+                resultado = valor1 / valor2;
+                System.out.println("Resultado Division: " + resultado);
+            }
+            case 4 -> {
+                resultado = valor1 * valor2;
+                System.out.println("Resultado Multiplicacion: " + resultado);
+            }
+            default ->
+                    System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
+        }
+    }//fin funcion ejecutarperacion
+
 }//fin clase
