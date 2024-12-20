3a1c8569ee4f53d401d5d708d3d0ea2355922552
Arturo Valdez <santiguardado27@gmail.com>
2024-12-19 18:55:46 -0600

    Condicionales de operacion

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -10,18 +10,43 @@ public class CalculadoraApp {
                 2.Resta
                 3.Division
                 4.Multiplicacion
+                5.Salir
                 """);
 
         System.out.println("Proporciona operacion que deseas realizar: ");
         var operador = Integer.parseInt(consola.nextLine());
-        System.out.print("Proporciona el primer valor: ");
-        var valor1 = Integer.parseInt(consola.nextLine());
-        System.out.print("Proporciona el segundo valor: ");
-        var valor2= Integer.parseInt(consola.nextLine());
-        var resultado = valor1 + valor2;
+        if( operador>=1  && operador <=4){
+            System.out.print("Proporciona el primer valor: ");
+            var valor1 = Integer.parseInt(consola.nextLine());
+            System.out.print("Proporciona el segundo valor: ");
+            var valor2= Integer.parseInt(consola.nextLine());
+            int resultado;
+            switch (operador) {
+                case 1 -> {
+                    resultado = valor1 + valor2;
+                    System.out.println("Resultado Suma: " + resultado);
+                }
+                case 2 -> {
+                    resultado = valor1 - valor2;
+                    System.out.println("Resultado Resta: " + resultado);
+                }
+                case 3 -> {
+                    resultado = valor1 / valor2;
+                    System.out.println("Resultado Division: " + resultado);
+                }
+                case 4 -> {
+                    resultado = valor1 * valor2;
+                    System.out.println("Resultado Multiplicacion: " + resultado);
+                }
 
-
-
-        System.out.println("Resultado: " + resultado);
+                default -> {
+                    System.out.println("Operacion '" + operador +"' incorrecto, seleccionar entre 1 y 4");
+                }
+            }
+        }else if(operador == 5){
+            System.out.println("Hasta pronto");
+        }else {
+            System.out.println("Operacion '" + operador +"' incorrecto, seleccionar entre 1 y 4");
+        }
     }
 }
