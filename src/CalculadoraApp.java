61a5bc16d673918ed9c4709222904672a2309998
Arturo Valdez <santiguardado27@gmail.com>
2024-12-19 19:11:01 -0600

    Uso de excepciones

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -16,40 +16,45 @@ public class CalculadoraApp {
                     """);
 
             System.out.print("Proporciona operacion que deseas realizar: ");
-            var operador = Integer.parseInt(consola.nextLine());
-            if (operador >= 1 && operador <= 4) {
-                System.out.print("Proporciona el primer valor: ");
-                var valor1 = Integer.parseInt(consola.nextLine());
-                System.out.print("Proporciona el segundo valor: ");
-                var valor2 = Integer.parseInt(consola.nextLine());
-                int resultado;
-                switch (operador) {
-                    case 1 -> {
-                        resultado = valor1 + valor2;
-                        System.out.println("Resultado Suma: " + resultado);
+            try {
+                var operador = Integer.parseInt(consola.nextLine());
+                if (operador >= 1 && operador <= 4) {
+                    System.out.print("Proporciona el primer valor: ");
+                    var valor1 = Integer.parseInt(consola.nextLine());
+                    System.out.print("Proporciona el segundo valor: ");
+                    var valor2 = Integer.parseInt(consola.nextLine());
+                    int resultado;
+                    switch (operador) {
+                        case 1 -> {
+                            resultado = valor1 + valor2;
+                            System.out.println("Resultado Suma: " + resultado);
+                        }
+                        case 2 -> {
+                            resultado = valor1 - valor2;
+                            System.out.println("Resultado Resta: " + resultado);
+                        }
+                        case 3 -> {
+                            resultado = valor1 / valor2;
+                            System.out.println("Resultado Division: " + resultado);
+                        }
+                        case 4 -> {
+                            resultado = valor1 * valor2;
+                            System.out.println("Resultado Multiplicacion: " + resultado);
+                        }
+                        default ->
+                                System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
                     }
-                    case 2 -> {
-                        resultado = valor1 - valor2;
-                        System.out.println("Resultado Resta: " + resultado);
-                    }
-                    case 3 -> {
-                        resultado = valor1 / valor2;
-                        System.out.println("Resultado Division: " + resultado);
-                    }
-                    case 4 -> {
-                        resultado = valor1 * valor2;
-                        System.out.println("Resultado Multiplicacion: " + resultado);
-                    }
-                    default -> System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
-                }
-            } else if (operador == 5) {
-                System.out.println("Hasta pronto");
-                break;
-            } else {
-                System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
-            }//fin else
-            //imprimir salto de linea
-            System.out.println("/n");
+                } else if (operador == 5) {
+                    System.out.println("Hasta pronto");
+                    break;
+                } else {
+                    System.out.println("Operacion '" + operador + "' incorrecto, seleccionar entre 1 y 4");
+                }//fin else
+                //imprimir salto de linea
+                System.out.println("/n");
+            } catch (Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
         }//fin de while
     }//fin main
 }//fin clase
